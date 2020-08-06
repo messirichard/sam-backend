d5869e7941bb9ca99e1c0b17005f494ba59f00aa
Richard <richardofay@gmail.com>
2020-08-06 16:08:01 +0700

    detete Dist

--- dist/controller/WidgetController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var WidgetController = /** @class */ (function () {
-    function WidgetController() {
-    }
-    return WidgetController;
-}());
-exports.default = WidgetController;
--- dist/entity/Widget.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Widget = /** @class */ (function () {
-    function Widget() {
-    }
-    return Widget;
-}());
-exports.default = Widget;
